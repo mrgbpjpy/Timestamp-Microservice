4b0a24d54a3fd97b0b47ac0c0191fc7217535b9e
mrgbpjpy <mrgbpjpy@gmail.com>
2024-05-18 18:40:47 +0000

    Initial commit of Timestamp Microservice

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-// index.js
-// where your node app starts
+const express = require('express');
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -15,7 +12,7 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 
@@ -23,10 +20,36 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+// Define a route for /api/:date?
+// Define a route for /api/:date?
+app.get('/api/:date?', (req, res) => {
+  // Get the date parameter from the request
+  const dateString = req.params.date;
+  let date;
 
+  // If date parameter is empty, use current time
+  if (!dateString) {
+      date = new Date();
+  } else {
+      // Check if the date string is a valid Unix timestamp
+      if (/^\d+$/.test(dateString)) {
+          date = new Date(parseInt(dateString));
+      } else {
+          // Attempt to parse the date
+          date = new Date(dateString);
+          
+          // Check if the date is invalid
+          if (isNaN(date.getTime())) {
+              return res.json({ error: "Invalid Date" });
+          }
+      }
+  }
 
-
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  // Return JSON object with unix and utc keys
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
